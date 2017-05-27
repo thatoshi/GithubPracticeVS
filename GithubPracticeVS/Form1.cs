ba61a1018c0c758dbc290a72a9e07028898b71f2
thatoshi <thatoshi@gmail.com>
2017-05-27 22:11:44 +0900

    Add a next script.

--- GithubPracticeVS/Form1.cs
+++ GithubPracticeVS/Form1.cs
@@ -25,6 +25,7 @@ namespace GithubPracticeVS
             Console.WriteLine("...may not be there.");
             Console.WriteLine("It happened so many times...");
             Console.WriteLine("Everything was like a...");
+            Console.WriteLine("dream, like a nightmare.");
             textBox1.Text += "We are X";
         }
     }
